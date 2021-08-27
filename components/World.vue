80c01fb93120c7f2a181cabe6c3539a003f4a75a
tog22 <tom.ash.github@philosofiles.com>
2021-08-27 12:35:47 -0700

    Start of World.data()

---
+++ components/World.vue
@@ -0,0 +1,40 @@
+<template>
+	<World_Viewer/>
+</template>
+
+<script>
+	import World_Viewer from './ui/World_Viewer.vue'
+	
+	export default {
+		name: 'World',
+		components: {
+			World_Viewer
+		},
+		data() {
+			return {
+				objects: [
+					// Locales, buildings et al get loaded here
+				],
+				next: 0,	// Next object id
+				c: -1,		// Current object id
+				cll: null,	// Currently loading locale
+				names: {
+					people: {
+						m: [
+							'Joseph',
+							'Joe',
+							'Frank',
+							'Stewart',
+							'Frederick',
+							'Andrew',
+							'The Donk'
+						]
+					}
+				}
+			}
+		},
+		mounted() {
+			this.objects[this.next] = 'Specification of first locale';
+		}
+	}
+</script>
