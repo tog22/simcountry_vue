6fd51c9a32442e0c4554a7f9ceb628540cc1a3a3
tog22 <tom.ash.github@philosofiles.com>
2021-08-29 14:50:41 -0700

    [mj] Displaying child buildings (step 2)

---
+++ components/Building.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="biz lumber_mill t" id="v1">
+		<img src="graphics/Buildings/SawMill.png">
+	</div>
+	<p>
+		Joseph's Lumber Mill
+	</p>
+</template>
+
+<script>
+	export default {
+		name: 'Building',
+		props: {
+			id: {
+				required: true,
+				type: Number
+			}
+		}
+	}
+</script>
