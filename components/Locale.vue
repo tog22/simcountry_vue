ab2ebb29857b2227cb91f0305e8844e91e913028
tog22 <tom.ash.github@philosofiles.com>
2021-08-29 12:34:12 -0700

    [mn] Dummy Locale

---
+++ components/Locale.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="visual">
+		<div class="temp">
+			<div class="locale grassland" id="v0">
+			
+				<div class="resources forest t">
+					<img src="graphics/nature/tree.png">
+					<img src="graphics/nature/tree.png">
+					<img src="graphics/nature/tree.png">
+					<img src="graphics/nature/tree.png">
+					<img src="graphics/nature/tree.png">
+					<img src="graphics/nature/tree.png">
+				</div>
+				
+				<div class="biz lumber_mill t" id="v1">
+					<img src="graphics/Buildings/SawMill.png">
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Locale',
+	components: {
+	}
+}
+</script>
---
+++ components/World_Viewer.vue
@@ -0,0 +1,14 @@
+<template>
+	<Locale/>
+</template>
+
+<script>
+	import Locale from './Locale'
+	
+	export default {
+		name: 'World_Viewer',
+		components: {
+			Locale
+		}
+	}
+</script>
