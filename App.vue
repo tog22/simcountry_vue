d7aae0e9fb9734cde37161de61a3dfe099fad250
tog22 <tom.ash.github@philosofiles.com>
2021-08-30 08:18:21 -0700

    Dummy control bar

--- App.vue
+++ App.vue
@@ -1,19 +1,22 @@
 <template>
-  <World/>
+	<Control_Bar/>
+	<World/>
 </template>
 
 <script>
-import '@/assets/ui.css';
-import '@/assets/map.css';
-
-import World from './components/World.vue'
-
-export default {
-  name: 'App',
-  components: {
-    World
-  }
-}
+	import '@/assets/ui.css';
+	import '@/assets/map.css';
+	
+	import World from './components/World.vue'
+	import Control_Bar from './components/Control_Bar.vue'
+	
+	export default {
+		name: 'App',
+		components: {
+			World,
+			Control_Bar
+		}
+	}
 </script>
 
 <style>
---
+++ components/Control_Bar.vue
@@ -0,0 +1,50 @@
+<template>
+	<div id="control_bar">
+		<div class="time_control control first">
+			<span id="play_pause" class="btn">
+				▶️ <!-- ⏸ -->
+			</span>
+			<span class="label">
+				<span id="timer">
+					Day <span id="day">1</span>
+				</span>
+			</span>
+		</div>
+		<div class="add_control control w_dropdown">
+			<div class="dropdown">
+				<div class="s_item" sc-action="add_lumber_mill" accesskey="f">
+						Lumber Mill
+				</div>
+				<div class="s_item" sc-action="add_farm">
+					Farm
+				</div>
+			</div>
+			<span class="btn">
+				⤵️
+			</span>
+			<!-- 
+			<div class="btn_ol_and_bg">
+				<img class="btn_bg" src="graphics/ui/button.png">
+				<div class="btn_ol">
+					+
+				</div>
+			</div>
+			-->
+			<span class="label">
+				Add
+			</span>
+		</div>		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Control_Bar',
+		components: {
+		}
+	}
+</script>
+
+<style>
+
+</style>
