8b740f27a24b4e9deed9814750fba6b84d27dcd9
tog22 <tom.ash.github@philosofiles.com>
2022-03-24 19:56:55 -0700

    New building setup

--- worlds/elements/buildings.js
+++ worlds/elements/buildings.js
@@ -5,31 +5,40 @@ import Building_Type from '@/model/Building_Type'
 
 export default function import_building_types() {
 	
-	w.btypes['Lumber Mill'] = new Building_Type('Lumber Mill');
-	w.btypes['Lumber Mill'].name = 'Lumber Mill'
-	w.btypes['Lumber Mill'].graphic = 'SawMill.png'
-	w.btypes['Lumber Mill'].resource_inputs['Trees'] = 1;
-	w.btypes['Lumber Mill'].outputs['Lumber'] = 1;
+	let lumber_mill = w.btypes['Lumber Mill'] = new Building_Type('Lumber Mill');
+	{
+		lumber_mill.name = 'Lumber Mill'
+		lumber_mill.graphic = 'SawMill.png'
+		lumber_mill.resource_inputs['Trees'] = 1;
+		lumber_mill.outputs['Lumber'] = 1;
+		// No build requirements
+	}
 	
-	w.btypes['Farm'] = new Building_Type('Farm');
-	w.btypes['Farm'].name = 'Farm'
-	w.btypes['Farm'].graphic = 'farm.png'
-	w.btypes['Farm'].outputs['Food'] = 4;
-	w.btypes['Farm'].build_requirements['Lumber'] = 20;
-	w.btypes['Farm'].resource_multipliers['soil quality'] = 1;
+	let farm = w.btypes['Farm'] = new Building_Type('Farm');
+	{
+		farm.name = 'Farm'
+		farm.graphic = 'farm.png'
+		farm.outputs['Food'] = 4;
+		farm.build_requirements['Lumber'] = 20;
+		farm.resource_multipliers.locale_aspects = {}
+		farm.resource_multipliers.locale_aspects['soil quality'] = 1; // todo: implement this
+	}
 	
-	w.btypes['Quarry'] = new Building_Type('Quarry');
-	w.btypes['Quarry'].name = 'Quarry'
-	w.btypes['Quarry'].graphic = 'StoneMine.png'
-	w.btypes['Quarry'].outputs['Stone'] = 1;
-	w.btypes['Quarry'].build_requirements['Lumber'] = 20;
+	let quarry = w.btypes['Quarry'] = new Building_Type('Quarry');
+	{
+			w.btypes['Quarry'].name = 'Quarry'
+		w.btypes['Quarry'].graphic = 'StoneMine.png'
+		w.btypes['Quarry'].outputs['Stone'] = 1;
+		w.btypes['Quarry'].build_requirements['Lumber'] = 20;
+	}
 	
-	w.btypes['Stone Toolmaker'] = new Building_Type('Stone Toolmaker');
-	w.btypes['Stone Toolmaker'].name = 'Stone Toolmaker'
-	w.btypes['Stone Toolmaker'].graphic = 'cottage_generic.png'
-	w.btypes['Stone Toolmaker'].labelled = true
-	w.btypes['Stone Toolmaker'].inputs['Stone'] = 1;
-	w.btypes['Stone Toolmaker'].outputs['Stone Tools'] = 1;
-	w.btypes['Stone Toolmaker'].build_requirements['Lumber'] = 20;
-	
+	let stone_toolmaker = w.btypes['Stone Toolmaker'] = new Building_Type('Stone Toolmaker');
+	{
+		w.btypes['Stone Toolmaker'].name = 'Stone Toolmaker'
+		w.btypes['Stone Toolmaker'].graphic = 'cottage_generic.png'
+		w.btypes['Stone Toolmaker'].labelled = true
+		w.btypes['Stone Toolmaker'].inputs['Stone'] = 1;
+		w.btypes['Stone Toolmaker'].outputs['Stone Tools'] = 1;
+		w.btypes['Stone Toolmaker'].build_requirements['Lumber'] = 20;
+	}
 }
